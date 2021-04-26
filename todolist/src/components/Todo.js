8f1c4639cc1f5647d804a3b767b70a26bb065680
lorenamateo90 <lorena.mateo.garcia@gmail.com>
2021-04-26 17:59:41 +0200

    add delete button

--- todolist/src/components/Todo.js
+++ todolist/src/components/Todo.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 
-const Todo = ({todo}) => {
+const Todo = ({todo, index, deleteTodo}) => {
     return (
         <>
-        <h3>{todo}</h3>
+          <div className="list">
+                <h3>{todo}</h3> <button className="btn-delete" onClick={() => deleteTodo(index)}>x</button>
+            </div>
     </>
     )
 }
